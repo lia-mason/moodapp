e572df58096c8d13eae8cf4c90e7b7b8da75b413
jrshawks <57079414+jrshawks@users.noreply.github.com>
2020-02-01 12:57:57 -0500

    Update backEndFunction.py

--- backEndFunction.py
+++ backEndFunction.py
@@ -1,45 +1,55 @@
 '''
 qHacks 2020
 '''
-#import libraries
+# import libraries
 from random import randint
 
-q = ["How are you?", "What are you doing today?"]
 
 class convo:
-    def _init_(self,questions,response, sScore):
-        self.questions = q #list of questions
+
+    def __init__(self):
+        questionLis = ["What is on your mind?", "How are you feeling today?", "If you could do one thing today what would it be?" , "What do you plan on doing today?", "Fill in question" ]
+
+        self.questions = questionLis  # list of questions
         self.response = []
-        self.sScore = 0 # range of +-0.25 on google sentiment analysis
-        
+        self.sScore = 0  # range of +-0.25 on google sentiment analysis
+        self.askingPeriod = False
 
     def ask(self):
-        i = randint(0,len(q)-1) #ask a random question from the list of questions
-        print(questions[i]) #googleTextToSpeech()replace with display and google speech GOOOOOOOOOOOOGLE
+        i = randint(0, len(self.questions) - 1)  # ask a random question from the list of questions
 
-    def reply(self):
-        speech = "This is the input from google speech to text"
-        self.response.append(speech)
+        question = self.questions[i]
+
+        print(question)  # googleTextToSpeech()replace with display and google speech GOOOOOOOOOOOOGLE
+
+        self.questions.remove(question)
+
+    def reply(self, response):
+        self.response.append(response)
 
     def getScore(self):
         temp = 0
         for i in self.response:
-             temp += 0#googleSentiment(i) GOOOOOOOOOOOOOOOOOGLE
-        sScore = temp/len(response)
+            temp += 0  # googleSentiment(i) GOOOOOOOOOOOOOOOOOGLE
+        sScore = temp / len(self.response)
         return sScore
+    def print_responses(self):
+        print(self.response)
+
+def main():
+    # if possible find a way to get name on first access and store
+    appOpen = True
+    Name = "Julia"
+    print("Hey", Name, "!")
+
+    while appOpen:
+        new_Convo = convo()
+
+        for x in range(0,5):
+            new_Convo.ask()
+            storeResponse = input("Response: ")
+            new_Convo.reply(storeResponse)
+        appOpen = False
 
 
-
-#if possible find a way to get name on first access and store
-appOpen = True
-Name = "Julia"
-print("Hey",Name,"!")
-
-while appOpen:
-    
-    newConvo= convo()
-
-
-
-        
-
+main()
